16d0da79c28f95099956528548e15dba032b3a8e
Alex Cristea <alex.p.cristea@gmail.com>
2025-07-30 15:28:06 +0300

    - Crated Pages and routes - Added index routes and nested routes where needed;

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import SideBar from "../components/SideBar";
+import styles from "./AppLayout.module.css";
 
 const AppLayout = () => {
   return (
-    <div>
-      <AppNav />
-      APP
+    <div className={styles.app}>
+      <SideBar />
+      <Map />
     </div>
   );
 };
